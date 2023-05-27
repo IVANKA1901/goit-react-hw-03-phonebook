e5558f4c35243ac5b6918693c48ec79f0301b0c4
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-05-27 17:42:28 +0200

    complete LS task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,26 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
+
   deleteContact = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(el => el.id !== contactId),
